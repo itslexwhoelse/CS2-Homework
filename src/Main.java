872e3997aadf1dbc403230dd1ef16f6b1e703d9b
ShapelessGolem <afdninja@gmail.com>
2023-08-31 00:54:47 -0400

    i did the homework yay

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi there, my name is lex!"); //simple println statement
+        System.out.println("this is another line too"); //ANOTHER!
+        System.out.println("hello? \nhow are you \nhelp me i am stuck under the water \ni am drowning \neuugugugu"); //using \n to be a new line on the same statement
+        System.out.println("and then i was all like \"no you didn't\" and then she was all \"yes i did\""); //girl no way
+        /*
+     Ex.1
+        Q: In computer jargon, what’s the difference between a statement and a comment?
+           A: a statement is a step of an algorithm and a comment is a little sticky note you put at the end of your code so you can remember what it does.
+        Q: What does it mean to say that a program is portable?
+            A: it runs on more than one type of computer.
+        Q: In common English, what does the word compile mean?
+            A: to put together code in a smaller, compressed, runnable format.
+        Q: What is an executable? Why is that word used as a noun?
+            A: an executable executes and does stuff, its like a file but instead of opening it, it just runs the code stored in there.
+
+     Ex.2
+        Type in the Hello World program; then compile and run it.
+        Add a print statement that displays a second message after the Hello, World!. Say something witty like, How are you?. Compile and run the program again.
+        Add a comment to the program (anywhere), recompile, and run it again. The new comment should not affect the result.
+
+            ok done lol
+
+    Ex.3
+        Remove one of the opening curly braces.
+        Remove one of the closing curly braces.
+        Instead of main, write mian.
+        Remove the word static.
+        Remove the word public.
+        Remove the word System.
+        Replace println with Println.
+        Replace println with print.
+        Delete one parenthesis.
+        Add an extra parenthesis.
+
+        alt shift lets you select multiple lines and type at once, just found that out so i could center all the text at once. cool
+      */
+    }
+}
